ff9c534637ebd6fd53ba14e98bb30cf8a3ba0545
lana-mrkonja <98597191+lana-mrkonja@users.noreply.github.com>
2023-05-31 15:13:42 +0200

    update app.R
    Added a theme, text in About tab, and started building a questionnaire

--- R/app.R
+++ R/app.R
@@ -1,47 +1,47 @@
 #
 # This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-#'@import shiny tidyverse
+
+#'@import shiny tidyverse bslib
 
 # library(shiny)
 # library(jsonlite)
 
 ui <- fluidPage(
   #titlePanel("ProjectExplicit"),
+  theme = bs_theme(bg = "#1F1D21",
+                   #17212E
+                   fg = "#ebebec",
+                   secondary = "#bc0031",
+                   primary="#E4CCC0",
+                   base_font = font_google("Prompt"),
+                   code_font = font_google("JetBrains Mono")
+                   ),
   navbarPage("ProjectExplicit",
-             tabPanel("About"),
+             tabPanel("About",
+                      mainPanel(includeMarkdown("about.md"))),
              navbarMenu("Experiments",
                         tabPanel("Stroop",
-                                 "This is a classic Stroop Task. It serves to measure your attentional contol",
-                                 actionButton("start_stroop", "Start Experiment")),
+                                 fluidRow(column(6,
+                                                 "This is a classic Stroop Task", br(),
+                                                 "It serves to measure your attentional control"),
+                                          column(6,
+                                                 actionButton("start_stroop", "Start Experiment")))),
                         tabPanel("Digit Span Task",
-                                 "This is a classic Digit Span Task. It serves to measure your working memory capacity",
-                                 actionButton("start_digit", "Start Experiment"))
-                        ),
+                                 fluidRow(column(6,
+                                                 "This is a classic Digit Span Task.", br(),
+                                                 "It serves to measure your working memory capacity"),
+                                          column(6,
+                                                 actionButton("start_digit", "Start Experiment")))),
+             ),
              navbarMenu("Questionnaires",
-                        tabPanel("Alcohol Use"),
+                        tabPanel("Alcohol Use",
+                                 radioButtons("Q1", "How often do you have a drink containing alcohol?",
+                                              c("0","1", "2","3","4"), choiceValues=c(0,1,2,3,4))),
                         tabPanel("Depression Symptoms")),
              tabPanel("Your Results"),
   ),
-  # selectInput(inputId = "start_exp",
-  #             label = "Choose Experiment",
-  #             choices = list(choose = "",
-  #                            Stroop = "experiments/Stroop/experiment/index.html",
-  #                            SDL = "experiments/SDL_final/experiment/index.html",
-  #                            VMAC = "experiments/VMAC_final/experiment/index.html"),
-  #             ),
-  # fileInput('target_upload', 'Choose file to upload',
-  #           accept = c(
-  #             'text/csv',
-  #             'text/comma-separated-values',
-  #             '.csv'
-  #           )),
+
   htmlOutput("experiment"),
   DT::dataTableOutput("sample_table")
 )
@@ -49,10 +49,6 @@ ui <- fluidPage(
 #  ----- server.R -----
 
 server <- function(input, output) {
-
-    # output$experiment<-renderUI({
-    #   return(includeHTML(input$start_exp))
-    # })
 
     #start the Stroop experiment Action Button:
     observeEvent(input$start_stroop, {
@@ -67,36 +63,12 @@ server <- function(input, output) {
         return(includeHTML("experiments/Stroop/experiment/index.html"))
       })
     })
-#
-#     df_products_upload <- reactive({
-#       inFile <- input$target_upload
-#       if (is.null(inFile))
-#         return(NULL)
-#       df <- read.csv(inFile$datapath, header = TRUE)
-#       return(df)
-#     })
-
-    # jspsych_data_upload <- reactive({
-    #   answers <- input$jspsych_results
-    #   if (is.null(answers))
-    #     return(NULL)
-    #   data <- jsonlite::fromJSON(answers)
-    #   return(data)
-    # })
-    #answers <- input$jspsych_results
-    #return(answers)
-    #data <- jspsych_data_upload()
 
     observeEvent(input$jspsych_results, {
       data <- jsonlite::fromJSON(input$jspsych_results)
       print(data$rt[1]) #switch to ID and then paste into datafile name so it doesn't overwrite
       write.csv(data, file = "C:/Users/mrkon/Documents/Experiments_Public_Github/ProjectExplicit/R/experiments/Stroop/data/test_outcomes.csv")
       })
-
-    # output$sample_table<- DT::renderDataTable({
-    #   #df <- df_products_upload()
-    #   DT::datatable(data)
-    # })
 }
 
 
@@ -105,8 +77,8 @@ server <- function(input, output) {
 #'
 #' @return
 #' @export
-#shinyApp(ui = ui, server = server)
-startApp <- function() {
-  shinyApp(ui = ui, server = server)
-}
+shinyApp(ui = ui, server = server)
+# startApp <- function() {
+#   shinyApp(ui = ui, server = server)
+# }
 #C:/Users/mrkon/Documents/R/win-library/4.1/ProjectExplicit
